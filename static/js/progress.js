0b0647ebb6d21553dcb05e5a78bfb9c7775342a1
blff122620 <blff122620@gmail.com>
2017-06-09 17:35:56 +0800

    优化process.js

--- static/js/progress.js
+++ static/js/progress.js
@@ -9,7 +9,10 @@ var myProgress = (function() {
             bgImageSuffix = "%, transparent 0px)", //这里是利用线性渐变实现的进度条，用这个方式的目的是让进度条的结构很简单，只有一层div就够了
             distance = 0, //进度条移动的距离，百分比
             left = 100, //剩下的距离，初始为100%
-            divisor = 100; //除数因子，越大平均速度越慢
+            divisor = 80, //除数因子，越大平均速度越慢
+            realOver = 0, //判断是不是真的页面载入完毕
+            simPoint = [{point:50,divisor:150},{point:80,divisor:300}],//设置一组速度变化的点
+            simPointIndex = 0;
         this.move = function() {
             if (!this.progressEle.style.backgroundImage) {
                 bgImage = window.getComputedStyle(this.progressEle).backgroundImage;
@@ -17,6 +20,12 @@ var myProgress = (function() {
                 bgImage = this.progressEle.style.backgroundImage;
             }
             if (distance < 100) {
+                if(!realOver && simPointIndex<simPoint.length && simPoint[simPointIndex]["point"] < distance){
+                    divisor = simPoint[simPointIndex++]["divisor"];
+                }
+                if(distance>95){
+                    divisor = 1000;//设置为极慢
+                }
                 distance += left / divisor; //根据除数因子求移动的距离，这样进度条走的速度会越来越慢，开始快，后面慢
                 left = 100 - distance; //剩下的距离要计算出来
                 bgImage = bgImagePrefix + distance + bgImageSuffix;
@@ -30,6 +39,7 @@ var myProgress = (function() {
         };
         this.over = function() { //进度条结束，马上到100%
             divisor = 1; //不要除数因子，简化计算
+            realOver = 1; //真的结束了
             left = 100 - distance; //直接把剩余路程走完
             var context = this;//保存this状态
             setTimeout(function() {
