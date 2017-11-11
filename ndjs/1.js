4009dbcedb2bcf139d395f61fb43493d60729f4d
blff122620 <blff122620@gmail.com>
2017-11-11 21:50:13 +0800

    fix: some fix

--- ndjs/1.js
+++ ndjs/1.js
@@ -1,13 +1,7 @@
-var name = "jay"; //一看这二逼就是周杰伦的死忠粉
-var person = {
-    name: "kang",
-    pro: {
-        name: "Michael",
-        getName: function() {
-            return this.name;
-        }
-    }
-};
-console.log(person.pro.getName());
-var pepole = person.pro.getName;
-console.log(pepole());
+class Foo {
+  // 写法一
+  prop = 2;
+
+  // 写法二
+  static prop = 2
+}
